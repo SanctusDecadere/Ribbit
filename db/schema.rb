7f25ba1d791d60c21e015d91001197d716947d57
Emilio Soto <emiliosoto4@yahoo.com>
2016-12-09 02:30:21 -0500

    created user resource, added validations to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161022214129) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",                                                            null: false
+    t.datetime "updated_at",                                                            null: false
+    t.string   "avatar_url"
+    t.string   "#<ActiveRecord::ConnectionAdapters::TableDefinition:0x007ff8cb566168>"
+  end
 
 end
