865de0ef1a306e7ca4610d560ece412a214c727a
Emilio Soto <emiliosoto4@yahoo.com>
2016-12-23 02:44:42 -0500

    creater usersController#new, #create methods; users/new view' git status pwd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+def new
+	@user = User.new
 end
+
+
+
+def create
+	@user = User.new(params[:user])
+	if @user.save
+		redirect_to @user, notice: "Thank you for sexing it up for Ribbit!"
+	else
+		render 'new'
+	end
+end
+
+end
+
+
+
